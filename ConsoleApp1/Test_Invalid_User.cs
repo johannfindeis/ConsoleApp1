99b9a951942c0584ae27c4ce9efebdf03719b28b
johannfindeis <44010596+johannfindeis@users.noreply.github.com>
2021-09-20 07:48:10 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Test_Invalid_User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace ConsoleApp1
+{
+    class Test_Invalid_User 
+    {
+        static void Main(string[] args)
+        {   
+
+            // invalid user test
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+            driver.FindElement(By.Name("user-name")).SendKeys("standard_user_invalid");
+
+            driver.FindElement(By.Name("password")).SendKeys("secret-sauce");
+            driver.FindElement(By.Name("login-button")).Click();
+
+
+            if (driver.Url == "https://www.saucedemo.com/inventory.html")
+            {
+                driver.Quit();
+            }
+            else
+            {
+                Console.WriteLine("test failed!");
+            }
+        }
+    }
+}
